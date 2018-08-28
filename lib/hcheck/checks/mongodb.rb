8f167108c7ec57fad5859eca648256d1960b7eb8
Rohit Joshi <link2j.roy@gmail.com>
2018-08-28 17:07:57 +0100

    Specs refactor

--- lib/hcheck/checks/mongodb.rb
+++ lib/hcheck/checks/mongodb.rb
@@ -6,7 +6,8 @@ module Hcheck
     module Mongodb
       # @config { host, port, options, tty, dbname, user, password }
       def status(config)
-        client = Mongo::Client.new(config['hosts'].compact, connect_timeout: 3, server_selection_timeout: config['hosts'].count * 2)
+        hosts = config['hosts'].compact
+        client = Mongo::Client.new(hosts, connect_timeout: 3, server_selection_timeout: hosts.count * 2)
         client.database_names
         client.close
         'ok'
@@ -14,14 +15,11 @@ module Hcheck
         client.close
         Hcheck.logger.error "[HCheck] Mongo::Error::NoServerAvailable #{e.message}"
         'bad'
-      rescue Errno::ECONNREFUSED => e
-        client.close
-        Hcheck.logger.error "[HCheck] Mongo::Error Connection refused #{e.message}"
-        'bad'
       end
 
       def self.included(_base)
         require 'mongo'
+        Mongo::Logger.level = Logger::INFO
       end
     end
   end
--- spec/hcheck/checks/redis_spec.rb
+++ spec/hcheck/checks/redis_spec.rb
@@ -4,30 +4,36 @@ RSpec.describe 'Hcheck::Checks::Redis' do
   describe '#status' do
     let(:config) do
       {
-        url: ENV['REDIS_DB_URL'],
-        db: ENV['REDIS_DB_NUM'],
-        password: ENV['REDIS_DB_PASSWORD']
+        url: 'REDIS_DB_URL',
+        db: 'REDIS_DB_NUM',
+        password: 'REDIS_DB_PASSWORD'
       }
+    end
+    let(:connection) { double('Redis', ping: 'pong') }
+
+    before do
+      allow(Redis).to receive(:new) { connection }
+    end
+
+    subject { status(config) }
+
+    it 'tries to make redis connection with supplied config' do
+      expect(Redis).to receive(:new).with(config)
+
+      subject
     end
 
     context 'when hcheck is able to connect to redis with supplied config' do
       it 'returns ok' do
-        allow_any_instance_of(Redis).to receive(:ping).and_return('pong')
-        expect(status(config)).to eql 'ok'
+        expect(subject).to eql 'ok'
       end
     end
 
     context 'when hcheck is not able to connect to redis with supplied config' do
-      let(:config) do
-        {
-          url: ENV['REDIS_DB_URL'],
-          db: ENV['REDIS_DB'],
-          password: ENV['REDIS_PASSWORD']
-        }
-      end
       it 'returns bad' do
-        allow_any_instance_of(Redis).to receive(:ping).and_raise(Redis::CannotConnectError)
-        expect(status(config)).to eql 'bad'
+        allow(connection).to receive(:ping).and_raise(Redis::CannotConnectError)
+
+        expect(subject).to eql 'bad'
       end
     end
   end
